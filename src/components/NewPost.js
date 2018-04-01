bbf27cab61f47b287863d0ce4a8bce75da48fd74
Charif Abdeslam <charifabdeslem@gmail.com>
2018-04-01 23:47:20 +0200

    Update

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,10 +1,28 @@
-import React , { Component } from 'react';
-
+import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
+import {Form, Button, Label, FormGroup, Container} from 'reactstrap';
 class NewPost extends Component {
-  render(){
-    return(
-  <h1>Form goes here</h1>
-    );
+  render() {
+    const {handleSubmit, pristine, submitting} = this.props;
+    return (<Container>
+      <Form onSubmit={this.handleSubmit}>
+        <FormGroup>
+          <Label>Title</Label>
+          <Field type="text" name="title" component="input"/>
+        </FormGroup>
+        <FormGroup>
+          <Label>Categories</Label>
+          <Field type="text" name="categories" component="input"/>
+        </FormGroup>
+        <FormGroup>
+          <Label>Content</Label>
+          <Field name="content" component="textarea"/>
+        </FormGroup>
+        <Button disabled={pristine || submitting}>Submit</Button>
+      </Form>
+    </Container>);
   }
 }
-export default NewPost;
+export default reduxForm({
+  form: 'NewPost'
+})(NewPost)
